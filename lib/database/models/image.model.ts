65a5afe76ecde8eb6921f4f0900bdfa2ef391fef
unknown <wlfriedtech97@gmail.com>
2025-03-18 13:39:25 +0100

    implement fixes

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,45 +1,43 @@
-import { create } from "domain";
-import { model, models, Schema,Document } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
-    title: string; // Der Titel des Bildes
-    transformationType: string; // Der Typ der Transformation
-    publicId: string; // Die öffentliche ID des Bildes
-    secureUrl: URL; // Die sichere URL des Bildes
-    width?: number; // Die Breite des Bildes (optional)
-    height?: number; // Die Höhe des Bildes (optional)
-    config?: object; // Konfigurationseinstellungen (optional)
-    transformationUrl?: string; // URL für die Transformation (optional)
-    aspectRatio?: string; // Das Seitenverhältnis (optional)
-    color?: string; // Die Farbe (optional)
-    prompt?: string; // Der Prompt (optional)
-    author: {
-        id: string;
-        firstName: string;
-        lastName: string;
-    }
-    createdAt?: Date; // Erstellungsdatum (optional, Standard: jetzt)
-    updatedAt?: Date; // Aktualisierungsdatum (optional, Standard: jetzt)
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: string; 
+  width?: number;
+  height?: number;
+  config?: object; 
+  transformationUrl?: string; 
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-
 const ImageSchema = new Schema({
-    title: { type: String, required: true },
-    transformationType: { type: String, required: true },
-    publicId: { type: String, required: true },
-    secureUrl: { type: URL, required: true },
-    width:{ types: Number},
-    height:{ types: Number},
-    config: { types: Object},
-    transformationUrl: { type: URL},
-    aspectRatio: { type: String},
-    color: { type: String},
-    prompt: { type: String},
-    author: { type: Schema.Types.ObjectId, ref: "User" },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureURL: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: String },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-const Image = models?.Image || model("Image", ImageSchema);
+const Image = models?.Image || model('Image', ImageSchema);
 
 export default Image;
