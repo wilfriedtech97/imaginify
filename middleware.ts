52488c49559a9e0dc50c3894d1bbcb46554b3915
unknown <wlfriedtech97@gmail.com>
2025-03-19 17:22:54 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: [ '/api/webhooks/clerk']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
+
 export const config = {
   matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',],
