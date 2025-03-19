af5244a84892d1535c2131f666476ac53de2f409
unknown <wlfriedtech97@gmail.com>
2025-03-19 09:20:52 +0100

    commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,22 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
-import React from 'react'
+import { Toaster } from '@/components/ui/toaster'
 
-const layout = ({ children }: { children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
       <MobileNav />
-        <div className="root-container">
-            <div className="wrapper">
-            {children}
-            </div>
+
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
         </div>
+      </div>
+      
+      <Toaster />
     </main>
   )
 }
 
-export default layout
+export default Layout
