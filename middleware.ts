90a14f25568e0669b50e401fd7587b9b5c779105
unknown <wlfriedtech97@gmail.com>
2025-03-19 17:58:58 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,5 @@ export default authMiddleware({
 });
  
 export const config = {
-  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
