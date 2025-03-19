22200455adcebc62f54c6ee39e164eaf9d70fc3a
unknown <wlfriedtech97@gmail.com>
2025-03-19 17:30:51 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
-
 export const config = {
   matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',],
